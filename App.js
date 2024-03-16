6570d74eaedb74940c0ef9e8003df12bc1d447d1
JawairiaZameer <163635364+JawairiaZameer@users.noreply.github.com>
2024-03-16 15:22:22 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import LoginPage from "./screens/LoginScreen/"
+import SignupPage from "./screens/SignupScreen"
+
+export default function App() {
+  return (
+    // <LoginPage />
+    <SignupPage />
+  );
+}
